530f792a47db0918a46212d75fc838445e700b3f
SabinSnider <sabinvsic@gmail.com>
2021-12-24 11:58:54 +0545

    1.2 commit - Added data, quiz questions

---
+++ app/data/QuizData.js
@@ -0,0 +1,17 @@
+export default data = [
+    {
+        question: "Which one is a mammal?",
+        options: ["Octopus, Cat, Pegion, Pikachu"],
+        correct_option: "Cat"
+    },
+    {
+        question: "Which one is a bird?",
+        options: ["Octopus, Cat, Pegion, Pikachu"],
+        correct_option: "Pigeon"
+    },
+    {
+        question: "Which one is a Fish?",
+        options: ["Dolphine, Cat, Pegion, Pikachu"],
+        correct_option: "Dolphine"
+    },
+]

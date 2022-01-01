9d07542033259f6943e305a88803608080d9f42b
SabinSnider <sabinvsic@gmail.com>
2022-01-01 18:55:29 +0545

    2.8.1 commit - added screenshots

---
+++ README.md
@@ -0,0 +1,4 @@
+#Quiz app
+Quiz app with Answer options and marks counter
+<Img src ="screenshots/Screenshot1.png" width="350">
+<Img src ="screenshots/Screenshot2.png" width="350">
--- app/data/QuizData.js
+++ app/data/QuizData.js
@@ -14,4 +14,19 @@ export default data = [
         options: ["Dolphine", "Cat", "Pigeon", "Pikachu"],
         correct_option: "Dolphine"
     },
+    {
+        question: "Which one is a Vehicle?",
+        options: ["Octopus", "Car", "Pistol", "Pikachu"],
+        correct_option: "Car"
+    },
+    {
+        question: "Which one is a Food?",
+        options: ["Momo", "Bike", "Kite", "Pikachu"],
+        correct_option: "Momo"
+    },
+    {
+        question: "Which one is a Japanese Dish?",
+        options: ["Donut", "Momo", "Ramen", "Pikachu"],
+        correct_option: "Ramen"
+    },
 ]
